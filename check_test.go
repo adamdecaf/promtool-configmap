decbdd3b35ac74eeded3b2ea83ecebd737f471a7
Adam Shannon <adamkshannon@gmail.com>
2018-05-02 15:06:57 -0500

    first pass at working code

---
+++ check_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCheck(t *testing.T) {
+	cases := []struct {
+		path string
+		valid bool
+	}{
+		{
+			path: "testdata/bad.yaml",
+			valid: false,
+		},
+		{
+			path: "testdata/bad.json",
+			valid: false,
+		},
+		{
+			path: "testdata/empty.yaml",
+			valid: false,
+		},
+		{
+			path: "testdata/good.yaml",
+			valid: true,
+		},
+		{
+			path: "testdata/good.json",
+			valid: true,
+		},
+	}
+	for i := range cases {
+		f, err := os.Open(cases[i].path)
+		if err != nil {
+			t.Fatalf("error reading %s, err=%v", cases[i].path, err)
+		}
+
+		err = check(f)
+		if cases[i].valid && err != nil {
+			t.Errorf("got error on %s, err=%v", cases[i].path, err)
+		}
+		if !cases[i].valid && err == nil {
+			t.Errorf("expected error on %s", cases[i].path)
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/prometheus/prometheus/pkg/rulefmt"
+	"gopkg.in/yaml.v2"
+)
+
+// https://v1-8.docs.kubernetes.io/docs/api-reference/v1.8/#configmap-v1-core
+
+// kind: ConfigMap
+// apiVersion: v1
+// data:
+//   blackbox.rules.yaml: |+
+
+// "kind": "ConfigMap",
+// "apiVersion": "v1",
+// "data": {
+//    "postgresql.rules.yaml": "..."
+
+type ConfigMap struct {
+	ApiVersion string            `json:"apiVersion", yaml:"apiVersion"`
+	Kind       string            `json:"kind", yaml:"kind"`
+	Data       map[string]string `json:"data", yaml:"data"`
+}
+
+func (c ConfigMap) validate() error {
+	// if c.ApiVersion != "v1" { // TODO(adam): why does this fail?
+	// 	return fmt.Errorf("unknown apiVersion %q", c.ApiVersion)
+	// }
+	if c.Kind != "ConfigMap" { // TODO(adam): flag to enable this?
+		return fmt.Errorf("got other k8s object %s", c.Kind)
+	}
+	if len(c.Data) == 0 {
+		return errors.New("empty ConfigMap")
+	}
+
+	// Check each yaml blob
+	for k,v := range c.Data {
+		groups, errs := rulefmt.Parse([]byte(v))
+		if len(errs) != 0 {
+			return errors.New("bad") // TODO(adam): strings.Join(errs, ", ") ... ? .Wrap?
+		}
+
+		if err := groups.Validate(); err != nil {
+			return fmt.Errorf("error validating %s, err=%v", k, err)
+		}
+	}
+	return nil
+}
+
+func main() {
+	if len(os.Args) == 1 || (len(os.Args) == 2 && strings.ToLower(os.Args[1]) == "help") {
+		showHelp()
+		os.Exit(1)
+	}
+
+	foundErrors := false
+
+	// TODO(adam): check -- for stdin, call check(os.Stdin) instead
+
+	// Assume each argument is a filepath
+	for i := range os.Args[1:] {
+		rawPath := os.Args[1:][i]
+		path, err := filepath.Abs(rawPath)
+		if err != nil {
+			foundErrors = true
+			fmt.Printf("ERROR attempting to read %s, err=%v\n", rawPath, err)
+			continue
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			foundErrors = true
+			fmt.Printf("ERROR opening %s, err=%v\n", path, err)
+			continue
+		}
+
+		if err := check(f); err != nil {
+			foundErrors = true
+			fmt.Printf("ERROR validating rules for %s, err=%v\n", path, err)
+			continue
+		}
+
+		fmt.Printf("%s passed checks\n", rawPath)
+	}
+
+	if foundErrors {
+		os.Exit(1)
+	}
+}
+
+func showHelp() {
+	fmt.Println("help") // TODO(adam)
+}
+
+func check(r io.Reader) error {
+	// be naive and read whole file
+	bs, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	var cfg ConfigMap
+
+	// read as json
+	err = json.Unmarshal(bs, &cfg)
+	if err == nil {
+		return cfg.validate()
+	}
+
+	// read as yaml
+	err = yaml.Unmarshal(bs, &cfg)
+	if err == nil {
+		return cfg.validate()
+	}
+
+	return errors.New("unable to parse file as json or yaml")
+}
