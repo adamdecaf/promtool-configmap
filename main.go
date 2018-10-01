cae8f2dd3ab904f283d18e36c206aa21e8816749
Adam Shannon <adamkshannon@gmail.com>
2018-10-01 16:45:25 -0500

    bump version for dev

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-const Version = "0.2.1"
+const Version = "0.3.0-dev"
 
 var (
 	flagVerbose = flag.Bool("verbose", false, "verbose output (show promtool output)")
